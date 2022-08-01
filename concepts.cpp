31db895117279ab2a9b8f725f95c4009b09c03a3
mabaro <markisbcn@gmail.com>
2022-08-01 11:19:07 +0200

    Create concepts.cpp

---
+++ concepts.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+concept equality_comparable = requires (T obj) {
+  { obj == obj } -> std::same_as<bool>;
+  { obj != obj } -> std::same_as<bool>;
+};
+
+struct vec2
+{
+    float x, y;
+
+    friend bool operator==(vec2 lhs, vec2 rhs)
+    {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    // operator!= not needed in C++20 due to operator rewrite rules!
+};
+
+static_assert(equality_comparable<vec2>);
